d7cd63e8afc627168991a330fe94e91793c1e48d
joserodriguez86 <josedelafuente_86@hotmail.com>
2021-08-20 14:29:05 -0300

    Modificaciones clase 1

--- clases/1/script-1.R
+++ clases/1/script-1.R
@@ -1,28 +1,22 @@
 #############################################
 # Curso ESA
-# Script clase 1: Comandos iniciales
+# Script clase 1: Breve introducción a R
 # Agosto 2021
 #############################################
 
-## Pedir ayuda
+#Ayuda -----------
 ?mean
 
 ?median
 
-## Tips
-
-### Para pedir ayuda en R
 help(solve)
-
-?solve
-
-help("[[")
 
 getRversion()
 
 ?help
 
-## Funciones y comandos elementales
+
+#Funciones y comandos elementales ---------------------------------------------------------------
 
 ### verificar los objetos disponibles en el espacio de trabajo
 ls()
@@ -31,13 +25,14 @@ ls()
 rm(cosa)
 
 ### retirar los objetos existentes en el área de trabajo
-rm (lista = ls ())
+rm(list = ls ())
 
 ### almacenar el espacio de trabajo
 save.image ()
 
 ### restaurar el espacio de trabajo
 load(".RData")
+
 ### si el archivo tiene un nombre diferente
 load("ilmiofile.Rdata")
 
@@ -50,7 +45,7 @@ loadhistory()
 setwd()
 
 ### ver el directorio de trabajo actual
-getwd ()
+getwd()
 
 ### si el comando es una expresión, R devuelve el resultado
 12 > 10
@@ -67,34 +62,38 @@ getwd ()
 
 4 * 3^3
 
-## Funciones matemáticas:
+## Funciones matemáticas
 ### raíz cuadrada
 sqrt(2)
 
 sqrt(sin(45 * pi/180))
 
 ### valor absoluto
-abs
+abs(-2)
+
 ### funciones trigonométricas
 sin(3.14159)
 
 sin(pi)
 
-cos
+cos(8)
 
-tan
+tan(92)
+
 ### funciones trigonométricas inversas
-asin
+asin(42)
 
-acos
+acos(64)
 
-atan
+atan(95)
+
 ### exponenciales y logaritmos naturales
-exp
+exp(643)
 
-log
+log(9232)
 
-## Asignaciones
+
+#Asignaciones-----------------------------------------------------------------------------
 ## guarda el resultado en un objeto con un nombre
 x <- sqrt(2)
 
@@ -114,210 +113,122 @@ x <= 10
 tf <- x > 10
 tf
 
-## Valores y vectores
+class(tf) # El comando class() nos sirve para saber qué tipo de objeto es
+
+#Valores y vectores --------------------------------------------------------------
 
 mi_valor <- 30 # número
 un_valor <- "vaca" # caracteres
 el_valor <- FALSE # booleano
 
 ### crear un vector
-c()
+c() # Comando para crear un vector
 
 x <- c(2, 3, 5, 7, 11)
 x
 
-mi_vector <- c(6,12,80,120)
-mi_vector <- c("María","Esteban","Juan")
+mi_vector1 <- c(6,12,80,120)
+mi_vector2 <- c("María","Esteban","Juan")
 
 ### si el vector contiene muchos elementos
-scan()
-
-### ejemplo 1
-x <- scan()
-1: 1
-2: 6
-3: 3
-4: 4
-5:
-
-x
-
-### ejemplo 2
-x <- scan()
-
-1: 23; 34; 32
-4: 33; 88; 44
-7:
-
+x <- scan() # Los valores deben escribirse en la consola y presionar "enter" entre cada valor
 x
 
 ## crear una secuencia de números de a a b
 ### ejemplo 1
 xx <- 1:10
-
 xx
+
 ### ejemplo 2
 xx <- 100:1
-
 xx
 
 ### crear vectores que contienen elementos repetidos
 rep(2, times = 3)
 
-a = c(rep(2, 3), 4, 5, rep(1, 5))
+a <- c(rep(2, 3), 4, 5, rep(1, 5))
 a
 
-
 ## operaciones aritméticas y lógicas básicas que se aplican a los escalares se pueden aplicar a los vectores.
-
 x <- 1:10
 x*2
 
-### elementos de un vector se pueden extraer
-
+### los elementos de un vector se pueden extraer
 xx[7]
 
 ### extraer varios elementos al mismo tiempo
-
 xx[c(2, 3, 5, 7, 11)]
 
 xx[85:91]
 
-### guardar en nuevos vectores:
-
+### guardar en nuevos vectores
 yy <- xx[c(1, 2, 4, 8, 16, 32, 64)]
 yy
 
 ### eliminar elementos correspondientes del vector
-
 x <- c(1, 2, 4, 8, 16, 32)
-x
 
-x[-4]
-### obtener todos los números en un rango
-
-mi_vector <- 1:20
-
-### repetir un valor específico
-
-repeated_values <- rep(23, times = 50)
+x <- x[-4]
 
 
-### indexar elementos de un vector
+### obtener el número de elementos en un vector o en una lista de objetos:
 
-mi_vector[5]
+length(xx)
 
-### obtener el número de lementos en un vector o en una lista de objetos:
 
-length(mi_vector)
-
-## Matrices
+#Matrices y listas --------------------------------------------------------------------------
 
 x <- matrix(c(2, 3, 5, 7, 11, 13), nrow = 3)
 x
 
-
-
-x <- matrix(c(2, 3, 5, 7, 11, 13), ncol = 2)
+y <- matrix(c(2, 3, 5, 7, 11, 13), ncol = 2)
 x
 
+z <- matrix(1:16, ncol = 4)
+z
 
-### array: matrices "multidimensionales"
+## matrices "multidimensionales"
+
+w <- 1:20
+ay <- array(w, dim = c(5, 2, 2))
 
 
-x <- 1:20
-ax <- array(x, dim = c(5, 2, 2)) > ax
+### la función dim() devuelve el tamaño (número de filas y número de columnas) de la matriz indicada como argumento.
 
-
-## las matrices se forman insertando distintos elementos por columna
-
-1,24,32,36,33
-2,16,44,34,33
-3,20,31,43,32
-4,23,35,37,35
-5,27,40,40,31
-6,19,43,32,37
-```
-
-### l contenido del archivo se puede adquirir y asignar a una matriz de 6 × 5 con los comandos:
-
-x2 <- scan("matdata", sep = ",")
-
-mx <- matrix(x2, ncol = 5, byrow = TRUE)
-mx
-
-### argumento byrow = TRUE especifica que el contenido debe adquirirse por fila y no por columna
-
-### función dim () devuelve el tamaño (número de filas y número de columnas) de la matriz indicada como argumento.
-
-dim (mx)
+dim(x)
 
 ### extraer una fila o columna completa
-x[, 1]
+x[, 1] #Extraigo primera columna
+x[3, ] #Extraigo tercera fila
 
-x[3, ]
 
-### extraer subconjuntos de filas y / o columnas.
-x <- matrix(1:16, ncol = 4)
-x
+### Creo una lista
+lista1 <- list(x,y,z)
 
 ### acceder a la información de una lista
 lista1[[1]]
-[1] 1 2 3
-
-
 lista1[[2]]
-[1] "A" "B" "C"
+lista1[[3]]
 
-### asignar un nombre a cada uno de los componentes de una lista
-lista2 <- list(comp1 = x1, comp2 = x2)
-lista2
 
-$comp1
-[1] 1 2 3
-
-$comp2
-[1] "A" "B" "C"
-
-lista2$comp2
-[1] "A" "B" "C"
-
-lista2$comp2[3]
-
-[1] "C"
-
-## Subsetting
+#Filtrado(Subsetting)----------------------------------------------------------------------------------
 mi_vector <- 1:10
 mi_vector[3:5] # extraer elementos
-nuevo_vector <- mi_vector[3:5] # asignar elementos a vector nuevo
+mi_vector[c(1,2,9)] # extraer valores específicos
 
-mi_vector[c(1,2,9)] # extraer
+nuevo_vector <- mi_vector[3:5] # asignar elementos a vector nuevo, funciona como filtro
 
-### usar la función c() para quitar algunos subsets
-mi_matriz[c(1:3,5),c(2,4)]
+### diferencia entre [[]] y [] en las listas
+nueva_lista <- lista1[1] # Devuelve la lista
 
-### diferencia entre [[]] y [] en las listas:
-nueva_lista <- mi_lista[1] # Devuelve la lista
-
-nuevo_valor <-  mi_lista[[1]] # Devuelve un objeto que estaba en la lista
+nuevo_valor <-  lista1[[1]] # Devuelve un objeto que estaba en la lista
 
 ### usar [] para el subset de una lista:
+nueva_lista <- lista1[1:2]
 
-nueva_lista <- mi_lista[1:2]
 
-## Subsetting inteligente
-which(mis_datos$Notas == "A")
-
-### crear un dataset con ciertas características
-A_estudiantes <- which(mis_datos$notas == "A")
-B_estudiantes <- which(mis_datos$notas == "B")
-estudiantes_dataset_reducido <- c(A_estudiantes, B_estudiantes)
-
-### seleccionar el vector para indexar únicamente algunas fila
-seleccion_data <- mis_datos[estudiantes_for_reduced_dataset,]
-
-## Cargar datos en R
-### Data Frames
+# Cargar datos en R ------------------------------------------------------------------------------
+## Data Frames------------------
 numero_estudiante <- c(1:10)
 
 notas <- c("A","B","C","A","C","F","D","B","B","A")
@@ -326,33 +237,50 @@ clase <- c(rep(0,times = 5),rep(1,times = 5))
 
 comida_gratis <- rep(TRUE,times = 10)
 
-### unir los vectores que hemos creado antes
+### unir los vectores que hemos creado antes en un data frame
 mis_datos <- data.frame(numero_estudiante,
                             notas,
                             clase,
                             comida_gratis,
                             stringsAsFactors = FALSE)
 
-### esignar el nombre de las columnas:
-nombrecol(mis_datos) <- c("numero_estudiante", "notas","clase","comida_gratis")
-
-### darle nombre a las filas:
-nombrefila(mis_datos) <- LETTERS[11:20]
+### asignar el nombre de las columnas:
+colnames(mis_datos) <- c("numero_estudiante", "notas","clase","comida_gratis")
+mis_datos #revisamos el data frame
 
 ### indexación funciona de la misma manera para las matrices
 mis_datos[,1]
 mis_datos[2,4]
 mis_datos$numero_estudiante # $ Indexación de columnas
-mis_datos$notas[3] # Tratar $ Indexación de columnas como vectores
+mis_datos$notas[3] # Indexación de columnas como vectores
 
+
+## Subsetting inteligente para data frames
+
+which(mis_datos$notas == "A")
+
+### crear un dataset con ciertas características
+A_estudiantes <- which(mis_datos$notas == "A")
+B_estudiantes <- which(mis_datos$notas == "B")
+estudiantes_dataset_reducido <- c(A_estudiantes, B_estudiantes)
+
+### seleccionar el vector para indexar únicamente algunas fila
+seleccion_data <- mis_datos[estudiantes_dataset_reducido,]
+
+### Otra manera sencilla es utilizar el comando subset()
+seleccion_data2 <- subset(mis_datos, notas == "A" | notas == "B")
+
+
+## Listas ----------------------------------------------------------------------------------
 ## Hacer una lista de estructuras de datos
-mi_lista <- vector(mode = "lista", length = 10)
+mi_lista <- vector(mode = "list", length = 10)
 
 ### crear una lista a partir de objetos
 mi_lista <- list(num = 10,
                 animal = "vaca",
                 vec = c(1:10),
                 dat = mis_datos)
+
 ## crear una lista vacía y luego llenarla
 mi_lista <- list()
 
@@ -361,21 +289,20 @@ mi_lista$dat = mis_datos
 mi_lista$cancion_favo = "Malamente"
 
 ### pegar listas al final de otras listas
-mi_lista <- append(mi_list, list(list(27,14,"perro")))
+mi_lista <- append(mi_lista, list(list(27,14,"perro")))
 
 ### observar el contenido de la lista:
-print(my_list)
+print(mi_lista)
 
-## Importar datos
+## Importar datos-----------------------------------------------------------
 ### Importar datos desde Excel
 install.packages("readxl") ## instalar paquete en la consola
 
 library(readxl) ## abrir librería del paquete
 
 ## asignar nombre a datos, pegar nombre del directorio
-encuesta <- read_excel("/Users/sofiajaime/Documents/rprojects/primer_proyecto/datos/encuesta1.xlsx")
-
-### este directorio es sólo un EJEMPLO.
+estaciones <- read_excel("bases/estaciones_tren.xls")
 
 ### comprobar que el trabajo se haya realizado bien
-encuesta ## aparecerán nuestros datos en la consola misma.
+estaciones ## aparecerán nuestros datos en la consola misma.
+View(estaciones) ## los datos se visualizarán en una nueva pestaña
